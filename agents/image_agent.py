ac47704023af28c4c53979eb10bae4630025227a
sashu14 <sashvithareddy.paila_2027@woxsen.edu.in>
2025-12-24 14:23:31 +0530

    Update image_agent.py

--- agents/image_agent.py
+++ agents/image_agent.py
@@ -1,1 +1,10 @@
+def analyze_image(image_bytes=None):
+    return {
+        "image_risk": "unknown"
+    }
+
+def analyze_image_from_pil(image):
+    return {
+        "image_risk": "unknown"
+    }
 
